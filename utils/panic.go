309c425513a8eacdada138151dee00cb5a500fd3
Don <tzy404270552@gmail.com>
2025-11-28 17:26:24 +0800

    :construction: add utils

--- utils/panic.go
+++ utils/panic.go
@@ -1,7 +1,19 @@
 package utils
 
+import (
+	"fmt"
+	"runtime/debug"
+)
+
 func PanicErr(err error) {
 	if err != nil {
 		panic(err)
+	}
+}
+
+func RecoverWithStack() {
+	if r := recover(); r != nil {
+		stack := debug.Stack()
+		fmt.Printf("panic: %v\n%s", r, string(stack))
 	}
 }
